340e3d9b5e930964812daf12fb0d3d84367963e8
Nnogga <27819512+nnogga@users.noreply.github.com>
2019-09-10 18:45:53 +0200

    initial mechagon

---
+++ BattleForAzeroth/MechagonCity.lua
@@ -0,0 +1,4 @@
+local dungeonIndex = 26
+MethodDungeonTools.dungeonTotalCount[dungeonIndex] = {normal=1,teeming=1,teemingEnabled=true}
+MethodDungeonTools.mapPOIs[dungeonIndex] = {}
+MethodDungeonTools.dungeonEnemies[dungeonIndex] = {}
---
+++ BattleForAzeroth/MechagonIsland.lua
@@ -0,0 +1,4 @@
+local dungeonIndex = 25
+MethodDungeonTools.dungeonTotalCount[dungeonIndex] = {normal=1,teeming=1,teemingEnabled=true}
+MethodDungeonTools.mapPOIs[dungeonIndex] = {}
+MethodDungeonTools.dungeonEnemies[dungeonIndex] = {}
