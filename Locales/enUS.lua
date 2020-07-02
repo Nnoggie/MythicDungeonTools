6afcc2f064aea51071d8cfe3cc551a1968a9b3ce
Nnogga <27819512+nnogga@users.noreply.github.com>
2020-07-02 03:09:34 +0200

    remove test entry

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -2,6 +2,5 @@ local addonName, MDT = ...
 MDT.L = {}
 local L = MDT.L
 
-L["Black Rook Hold"] = "Test"
 
 --@localization(locale="enUS", format="lua_additive_table", namespace="MDT", handle-subnamespaces="none")@
