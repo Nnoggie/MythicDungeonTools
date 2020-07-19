1ddc73d9982edcdea677d47ce8692a231a032476
Nnogga <27819512+nnogga@users.noreply.github.com>
2020-07-19 18:37:09 +0200

    table init

--- Shadowlands/SanguineDepths.lua
+++ Shadowlands/SanguineDepths.lua
@@ -6,3 +6,5 @@ MDT.mapInfo[dungeonIndex] = {
 };
 
 MDT.dungeonTotalCount[dungeonIndex] = {normal=1000,teeming=1000,teemingEnabled=true}
+MDT.mapPOIs[dungeonIndex] = {}
+MDT.dungeonEnemies[dungeonIndex] = {}
