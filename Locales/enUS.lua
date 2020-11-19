05535ff7aea8b381444bf2c333ffe364f15f93ef
Nnogga <27819512+nnogga@users.noreply.github.com>
2020-11-19 03:36:54 +0100

    mapping updates

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -1026,4 +1026,4 @@ L["Remnant of Fury"] = "Remnant of Fury"
 
 L["plaguefallDevotedNote"] = "Plaguefall Devoted which have already been transformed by Oozing Carcass before engaging Ickor Bileflesh do not award Enemy Forces."
 L["ConnectedTip"] = "Group connections in MDT do not reflect if NPCs are linked together in the game world."
-
+L["theaterOfPain_miniBossNote"] = "Only one duelist will be alive."
