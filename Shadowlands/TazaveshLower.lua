b71b148ff4821415e65cd89405839b5be4d102e2
Nnogga <27819512+nnogga@users.noreply.github.com>
2021-12-05 00:58:27 +0100

    tazavesh map links

--- Shadowlands/TazaveshLower.lua
+++ Shadowlands/TazaveshLower.lua
@@ -12,4 +12,69 @@ MDT.mapInfo[dungeonIndex] = {
 };
 
 MDT.dungeonTotalCount[dungeonIndex] = {normal=160,teeming=192,teemingEnabled=true}
-MDT.mapPOIs[dungeonIndex] = {}
+
+MDT.mapPOIs[dungeonIndex] = {
+    [1] = {
+       [1] = {
+          ["y"] = -353.24711814285;
+          ["x"] = 366.3568478256;
+          ["connectionIndex"] = 1;
+          ["target"] = 2;
+          ["type"] = "mapLink";
+          ["template"] = "MapLinkPinTemplate";
+          ["direction"] = 2;
+       };
+       [2] = {
+          ["y"] = -237.96954332336;
+          ["x"] = 333.46579433805;
+          ["connectionIndex"] = 2;
+          ["target"] = 3;
+          ["type"] = "mapLink";
+          ["template"] = "MapLinkPinTemplate";
+          ["direction"] = -2;
+       };
+       [3] = {
+          ["y"] = -154.10258483958;
+          ["x"] = 347.49727301494;
+          ["connectionIndex"] = 3;
+          ["target"] = 4;
+          ["type"] = "mapLink";
+          ["template"] = "MapLinkPinTemplate";
+          ["direction"] = -2;
+       };
+    };
+    [2] = {
+       [1] = {
+          ["y"] = -107.06677772897;
+          ["x"] = 310.27427710206;
+          ["connectionIndex"] = 1;
+          ["target"] = 1;
+          ["type"] = "mapLink";
+          ["template"] = "MapLinkPinTemplate";
+          ["direction"] = -2;
+       };
+    };
+    [3] = {
+       [1] = {
+          ["y"] = -359.07397950769;
+          ["x"] = 662.06461673007;
+          ["connectionIndex"] = 2;
+          ["target"] = 1;
+          ["type"] = "mapLink";
+          ["template"] = "MapLinkPinTemplate";
+          ["direction"] = 2;
+       };
+    };
+    [4] = {
+       [1] = {
+          ["y"] = -487.26258086352;
+          ["x"] = 531.69093773083;
+          ["connectionIndex"] = 3;
+          ["target"] = 1;
+          ["type"] = "mapLink";
+          ["template"] = "MapLinkPinTemplate";
+          ["direction"] = 2;
+       };
+    };
+ };
+ 
--- Shadowlands/TazaveshUpper.lua
+++ Shadowlands/TazaveshUpper.lua
@@ -12,4 +12,88 @@ MDT.mapInfo[dungeonIndex] = {
 };
 
 MDT.dungeonTotalCount[dungeonIndex] = {normal=160,teeming=192,teemingEnabled=true}
-MDT.mapPOIs[dungeonIndex] = {}
+MDT.mapPOIs[dungeonIndex] = {
+    [1] = {
+       [1] = {
+          ["y"] = -300.66597086773;
+          ["x"] = 259.58803612169;
+          ["connectionIndex"] = 1;
+          ["target"] = 2;
+          ["type"] = "mapLink";
+          ["template"] = "MapLinkPinTemplate";
+          ["direction"] = -1;
+       };
+    };
+    [2] = {
+       [1] = {
+          ["y"] = -482.16413322051;
+          ["x"] = 176.25896157079;
+          ["connectionIndex"] = 1;
+          ["target"] = 1;
+          ["type"] = "mapLink";
+          ["template"] = "MapLinkPinTemplate";
+          ["direction"] = 1;
+       };
+       [2] = {
+          ["y"] = -297.16465402191;
+          ["x"] = 538.2460884279;
+          ["connectionIndex"] = 2;
+          ["target"] = 3;
+          ["type"] = "mapLink";
+          ["template"] = "MapLinkPinTemplate";
+          ["direction"] = 2;
+       };
+    };
+    [3] = {
+       [1] = {
+          ["y"] = -133.28719090954;
+          ["x"] = 152.95188346973;
+          ["connectionIndex"] = 2;
+          ["target"] = 2;
+          ["type"] = "mapLink";
+          ["template"] = "MapLinkPinTemplate";
+          ["direction"] = -2;
+       };
+       [2] = {
+          ["y"] = -320.47170086583;
+          ["x"] = 534.60425588888;
+          ["connectionIndex"] = 3;
+          ["target"] = 4;
+          ["type"] = "mapLink";
+          ["template"] = "MapLinkPinTemplate";
+          ["direction"] = -1;
+       };
+    };
+    [4] = {
+       [1] = {
+          ["y"] = -200.29488416438;
+          ["x"] = 660.60781085846;
+          ["connectionIndex"] = 3;
+          ["target"] = 3;
+          ["type"] = "mapLink";
+          ["template"] = "MapLinkPinTemplate";
+          ["direction"] = 1;
+       };
+       [2] = {
+          ["y"] = -345.9637472103;
+          ["x"] = 364.90002325125;
+          ["connectionIndex"] = 4;
+          ["target"] = 5;
+          ["type"] = "mapLink";
+          ["template"] = "MapLinkPinTemplate";
+          ["direction"] = -2;
+       };
+    };
+    [5] = {
+       [1] = {
+          ["y"] = -299.3496847796;
+          ["x"] = 698.48184402983;
+          ["connectionIndex"] = 4;
+          ["target"] = 4;
+          ["type"] = "mapLink";
+          ["template"] = "MapLinkPinTemplate";
+          ["direction"] = 2;
+       };
+    };
+ };
+ 
