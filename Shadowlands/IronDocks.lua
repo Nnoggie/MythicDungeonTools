c397cf5380a9721cd097cb5e984084122e0eee14
Nnogga <27819512+nnogga@users.noreply.github.com>
2022-07-27 17:02:48 +0200

    zoom + graveyard

--- Shadowlands/IronDocks.lua
+++ Shadowlands/IronDocks.lua
@@ -2,10 +2,6 @@ local MDT = MDT
 local L = MDT.L
 local dungeonIndex = 41
 MDT.dungeonList[dungeonIndex] = L["IronDocks"]
-MDT.mapInfo[dungeonIndex] = {
-    
-};
-
 MDT.dungeonMaps[dungeonIndex] = {
   [0] = "IronDocks",
   [1] = "IronDocks1_",
@@ -13,11 +9,27 @@ MDT.dungeonMaps[dungeonIndex] = {
 MDT.dungeonSubLevels[dungeonIndex] = {
    [1] = L["IronDocks"],
 }
-
-MDT.mapPOIs[dungeonIndex] = {
-
+MDT.mapInfo[dungeonIndex] = {
+  viewportPositionOverrides = {
+    [1] = {
+      zoomScale = 1.2999999523163;
+      horizontalPan = 193.84615203593;
+      verticalPan = 39.418697423882;
+    };
+  }
 };
 
+MDT.mapPOIs[dungeonIndex] = {
+  [1] = {
+     [1] = {
+        ["y"] = -246.22239132748;
+        ["x"] = 248.70555745877;
+        ["template"] = "DeathReleasePinTemplate";
+        ["graveyardDescription"] = "";
+        ["type"] = "graveyard";
+     };
+  };
+};
 
  MDT.dungeonTotalCount[dungeonIndex] = {normal=1000,teeming=1000,teemingEnabled=true}
  MDT.dungeonEnemies[dungeonIndex] = {}
