228bb0ec0783baa8e65901691d1e2b1aaf86347a
Stinth <malthejelstrup@gmail.com>
2022-08-01 12:39:07 +0200

    Local affix rotation handling with UpdateAffixWeek

--- MythicDungeonTools.lua
+++ MythicDungeonTools.lua
@@ -122,6 +122,7 @@ local defaultSavedVars = {
       numberCustomColors = 12,
     },
     selectedDungeonList = 4,
+    knownAffixWeeks = {},
   },
 }
 do
@@ -238,6 +239,56 @@ local affixWeeks = {
   [11] = { 0, 0, 9, 131 }, -- inspiring quaking tyrannical shrouded
   [12] = { 0, 0, 10, 131 }, -- sanguine grievous fortified shrouded
 }
+
+function MDT:UpdateAffixWeeks()
+    -- The idea is to know the season start date: season_start
+    -- Then from current time calculate which week in the affix rotation is live
+    -- By doing this weekly you can populate the entire affix weeks rotation
+    if not IsAddOnLoaded("Blizzard_ChallengesUI") then
+       LoadAddOn("Blizzard_ChallengesUI")
+    end
+    -- copied from MDT:GetCurrentAffixWeek not sure if necessary
+    C_MythicPlus.RequestCurrentAffixes()
+    C_MythicPlus.RequestMapInfo()
+    C_MythicPlus.RequestRewards()
+
+    local season_start = { -- Edit this at the start of the season - IMPORTANT: This is North America launch in PST time
+       ["year"] = 2022,
+       ["month"] = 3,
+       ["day"] = 1,
+       ["hour"] = 8 -- You'll never need to change this
+    }
+
+    -- Find current week index
+    local hour = 3600
+    local one_week = 604800
+    local offset = time()-time(date("!*t")) + 7 * hour -- offset between na and utc
+    local utc_na_season_start = time(season_start) + offset -- na regional season start time in utc time
+    local utc_region_week_start = GetServerTime() - (one_week - C_DateAndTime.GetSecondsUntilWeeklyReset()) + 20 -- players regional weekly start time in utc
+    -- 20 second buffer because GetSecondsUntilWeeklyReset is not precise
+    local region_offset = (utc_region_week_start - utc_na_season_start) % one_week -- difference between na reset time and players regional reset time
+    local elapsed_season = utc_region_week_start - (utc_na_season_start + region_offset) -- total time since season start
+    local current_week_idx = math.floor(elapsed_season / one_week) % #affixWeeks + 1 -- weekly affix rotation index of current week for player
+
+    -- Save current weeks affixes to db
+    local current_affixes = C_MythicPlus.GetCurrentAffixes()
+    db.knownAffixWeeks[current_week_idx] = {}
+    for k,idx in pairs({2,3,1,4}) do
+        tinsert(db.knownAffixWeeks[current_week_idx], current_affixes[idx]["id"])
+    end
+
+    -- return if no seasonal affix is found
+    if not db.knownAffixWeeks[current_week_idx][4] then return end
+
+    -- Replace affixWeeks with data from db
+    for week_idx, known_affixes in pairs(db.knownAffixWeeks) do
+        -- ensure seasonal affix from db matches current week
+        if known_affixes[4] == db.knownAffixWeeks[current_week_idx][4] then
+            affixWeeks[week_idx] = known_affixes
+        end
+    end
+end
+
 MDT.mapInfo = {}
 MDT.dungeonTotalCount = {}
 MDT.scaleMultiplier = {}
@@ -650,6 +701,7 @@ function MDT:MakeTopBottomTextures(frame)
 end
 
 function MDT:MakeSidePanel(frame)
+  MDT:UpdateAffixWeeks()
 
   if frame.sidePanel == nil then
     frame.sidePanel = CreateFrame("Frame", "MDTSidePanel", frame)
