6f57f3f20863d8ff20ced76347fa409ca1519d98
Nnoggie <27819512+nnogga@users.noreply.github.com>
2022-08-05 19:00:57 +0200

    formatting

--- MythicDungeonTools.lua
+++ MythicDungeonTools.lua
@@ -131,7 +131,8 @@ local defaultSavedVars = {
 do
   for i = 1, 80 do
     defaultSavedVars.global.presets[i] = {
-      [1] = { text = L["Default"], value = {}, objects = {}, colorPaletteInfo = { autoColoring = true, colorPaletteIdx = 4 } },
+      [1] = { text = L["Default"], value = {}, objects = {},
+        colorPaletteInfo = { autoColoring = true, colorPaletteIdx = 4 } },
       [2] = { text = L["<New Preset>"], value = 0 },
     }
     defaultSavedVars.global.currentPreset[i] = 1
