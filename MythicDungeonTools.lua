e9f3d27a79c203f4dbbf32388530102e9cacc1c7
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2023-09-05 05:07:02 +0200

    WagoAnalytics

--- MythicDungeonTools.lua
+++ MythicDungeonTools.lua
@@ -109,6 +109,8 @@ function SlashCmdList.MYTHICDUNGEONTOOLS(cmd, editbox)
   end
 end
 
+MDT.WagoAnalytics = LibStub("WagoAnalytics"):Register("rN4VrAKD")
+
 function MDT:GetLocaleIndex()
   local localeToIndex = {
     ["enUS"] = 1,
