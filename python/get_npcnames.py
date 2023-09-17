899e02f96ed157441616f6b1e76a33ae1c321812
Stinth <malthejelstrup@gmail.com>
2023-09-18 00:07:34 +0200

    formatting

--- python/get_npcnames.py
+++ python/get_npcnames.py
@@ -1,14 +1,15 @@
 # Created by Urtgard
 # Scrapes wowhead for npcNames based on npcIds
-import urllib.request
-from urllib.error import HTTPError
-import re
-import os
-from socket import error as SocketError
-import threading
 import math
+import os
+import re
+import threading
 import time
 from collections import OrderedDict
+from html import unescape
+from socket import error as SocketError
+
+import requests
 
 # Usage:
 # 1. Refer to requirements.txt to install required packages
@@ -18,9 +19,10 @@ from collections import OrderedDict
 
 # wowhead is picky
 browser_headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
 }
+
 
 def get_npc_name(npc_id, language, ptr=False):
     try:
@@ -46,73 +48,84 @@ def get_npc_name(npc_id, language, ptr=False):
             time.sleep(5)
             return get_npc_name(npc_id, language)
 
+
 npc_list = {}
 languages = {
-    'frFR': 'fr',
-    'deDE': 'de',
-    'itIT': 'it',
-    'koKR': 'ko',
-    'zhCN': 'cn',
-    'ruRU': 'ru',
-    'esES': 'es',
-    'ptBR': 'pt',
+    "frFR": "fr",
+    "deDE": "de",
+    "itIT": "it",
+    "koKR": "ko",
+    "zhCN": "cn",
+    "ruRU": "ru",
+    "esES": "es",
+    "ptBR": "pt",
 }
-expansions = ['Legion', 'BattleForAzeroth', 'Shadowlands', 'Dragonflight']
+expansions = ["Legion", "BattleForAzeroth", "Shadowlands", "Dragonflight"]
+
 
 def get_npc_names_localized(npc_id):
-    name = get_npc_name(npc_id, 'en')
+    name = get_npc_name(npc_id, "en")
     if not name:
-        print(f'Failed to get EN name for {npc_id}')
+        print(f"Failed to get EN name for {npc_id}")
         return
     if name in npc_list:
-        print(f'{name} is already in list, skipping ID {npc_id}')
+        print(f"{name} is already in list, skipping ID {npc_id}")
         return
-    npc_list[name] = {'enUS': name}
+    npc_list[name] = {"enUS": name}
     for lang in languages:
         npc_list[name][lang] = get_npc_name(npc_id, languages[lang])
+
 
 def collect_mdt_npcids():
     # reaping and awakening mobs
     ids = [148716, 148893, 148894, 161169, 160984, 161164, 161167]
     for expansion in expansions:
-        for filename in os.listdir('{}'.format(expansion)):
+        for filename in os.listdir("{}".format(expansion)):
             if not filename.endswith(".lua"):
                 continue
-            f = open('{}\\{}'.format(expansion,filename))
+            f = open("{}\\{}".format(expansion, filename))
             line = f.readline()
             while line:
-                npc_id = re.search('\[\"id\"\] = (\d*)', line)
+                npc_id = re.search('\["id"\] = (\d*)', line)
                 if npc_id:
                     npc_id = int(npc_id.group(1))
                     if npc_id > 100000000:
-                        print(f'Invalid NPC ID {npc_id}, skipping')
-                    elif not npc_id in ids:
+                        print(f"Invalid NPC ID {npc_id}, skipping")
+                    elif npc_id not in ids:
                         ids.append(npc_id)
                 line = f.readline()
     return ids
 
+
 finished_ids = 0
 total_ids = 0
 lock = threading.Lock()
+
+
 def get_npc_names(ids):
     for id in ids:
         get_npc_names_localized(id)
         lock.acquire()
         global finished_ids
         finished_ids = finished_ids + 1
-        print(f'Finished {finished_ids}/{total_ids}')
+        print(f"Finished {finished_ids}/{total_ids}")
         lock.release()
 
+
 def write_to_lua(dst, src):
-    file = f'{dst}.lua'
-    with open(file, 'w', encoding='UTF-8') as out:
+    file = f"{dst}.lua"
+    with open(file, "w", encoding="UTF-8") as out:
         for npc in npc_list:
-            npc_name_localized = re.match('\[?([^\]]*)', str(npc_list[npc][src])).group(1)
+            npc_name_localized = re.match("\[?([^\]]*)", str(npc_list[npc][src])).group(
+                1
+            )
             out.write(f'\tL["{npc}"] = "{npc_name_localized}"\n')
+
 
 def split_list(a, n):
     k, m = divmod(len(a), n)
-    return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
+    return (a[i * k + min(i, m) : (i + 1) * k + min(i + 1, m)] for i in range(n))
+
 
 if __name__ == "__main__":
     # Make sure initial working directory is MythicDungeonTools
@@ -122,8 +135,8 @@ if __name__ == "__main__":
         os.chdir("..")
     ids = collect_mdt_npcids()
     total_ids = len(ids)
-    print(f'{total_ids} NPC IDs in dungeon files')
-    nthreads = 5 #100
+    print(f"{total_ids} NPC IDs in dungeon files")
+    nthreads = 5  # 100
     chunks = list(split_list(ids, nthreads))
     threads = []
     for i in range(0, nthreads):
@@ -136,6 +149,6 @@ if __name__ == "__main__":
     for lang in languages:
         write_to_lua(lang, lang)
     # scuffed languages
-    write_to_lua(src='enUS', dst='enUS')
-    write_to_lua(src='esES', dst='esMX')
-    write_to_lua(src='zhCN', dst='zhTW')
+    write_to_lua(src="enUS", dst="enUS")
+    write_to_lua(src="esES", dst="esMX")
+    write_to_lua(src="zhCN", dst="zhTW")
