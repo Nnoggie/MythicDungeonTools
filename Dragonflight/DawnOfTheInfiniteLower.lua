3186d84826be5b82edce5dc2a1871b197a8ac122
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2023-09-26 13:42:44 +0200

    doti lower sublevels

--- Dragonflight/DawnOfTheInfiniteLower.lua
+++ Dragonflight/DawnOfTheInfiniteLower.lua
@@ -7,6 +7,11 @@ MDT.mapInfo[dungeonIndex] = {
 
 };
 
+local zones = { 2190, 2191, 2192, 2193, 2194, 2195, 2196, 2197, 2198 }
+for _, zone in ipairs(zones) do
+  MDT.zoneIdToDungeonIdx[zone] = dungeonIndex
+end
+
 local function getSublevelMap(frame, sublevel)
   for i = 1, 12 do
     if frame["mapPanelTile"..i] then
