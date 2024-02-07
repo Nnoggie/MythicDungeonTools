3de116739613f9c39ed68e21fc3b0dfea74092f0
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2024-02-07 15:41:15 +0100

    remove fear from shieldbearers

--- BattleForAzeroth/AtalDazar.lua
+++ BattleForAzeroth/AtalDazar.lua
@@ -1103,7 +1103,6 @@ MDT.dungeonEnemies[dungeonIndex] = {
       ["Grip"] = true,
       ["Shackle Undead"] = true,
       ["Root"] = true,
-      ["Fear"] = true,
       ["Banish"] = true,
       ["Disorient"] = true,
       ["Repentance"] = true,
