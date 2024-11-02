4cf49d185bc763bf42b1a67cc34d0cdcfbb0e2c3
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2024-11-02 18:05:52 +0100

    Rename Starved Crawler

--- TheWarWithin/AraKara.lua
+++ TheWarWithin/AraKara.lua
@@ -70,7 +70,7 @@ MDT.mapPOIs[dungeonIndex] = {
 
 MDT.dungeonEnemies[dungeonIndex] = {
   [1] = {
-    ["name"] = "Starved Crawler",
+    ["name"] = "Ravenous Crawler",
     ["id"] = 216336,
     ["count"] = 1,
     ["health"] = 5092182,
