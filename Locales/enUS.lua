6cd39b19db37ce824afab8463456f0de441752b6
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2025-06-22 02:40:17 +0200

    add s3 name

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -3,6 +3,7 @@ local addonName, MDT = ...
 local L = MDT.L
 L = L or {}
 
+L["The War Within Season 3"] = "War Within Season 3"
 L["MDT: Cannot add enemy - you are trying to add too many enemies of the same kind"] = "MDT: Cannot add enemy - you are trying to add too many enemies of the same kind"
 L["Clear all Markers"] = "Clear all Markers"
 L["Okay"] = "Okay"
