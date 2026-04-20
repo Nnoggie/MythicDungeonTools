cf111f9b33133913f4df75c7ad7d94a74cd79353
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2026-04-21 01:36:37 +0200

    Fix Seat zone map id

--- Midnight/SeatoftheTriumvirate.lua
+++ Midnight/SeatoftheTriumvirate.lua
@@ -10,7 +10,7 @@ MDT.mapInfo[dungeonIndex] = {
   mapID = 239
 };
 
-local zones = { 2097, 2098, 2099 }
+local zones = { 903 }
 for _, zone in ipairs(zones) do
   MDT.zoneIdToDungeonIdx[zone] = dungeonIndex
 end
