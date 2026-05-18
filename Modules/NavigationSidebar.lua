459de41980934426d72a9b83f9d9968e2bfecc48
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2026-05-18 17:37:32 +0200

    Swap macro and focus marker sidebar icons

--- Modules/NavigationSidebar.lua
+++ Modules/NavigationSidebar.lua
@@ -147,8 +147,8 @@ function MDT:MakeNavigationSidebar(frame)
 
     local sections = {
       { key = "maps", tooltip = L["Maps"], texCoords = { 0, 0.25, 0, 0.25 } },
-      { key = "macros", tooltip = L["Macros"], texCoords = { 0.5, 0.75, 0, 0.25 } },
-      { key = "marks", tooltip = L["Focus Marker Assignments"], texCoords = { 0.25, 0.5, 0, 0.25 } },
+      { key = "marks", tooltip = L["Focus Marker Assignments"], texCoords = { 0.5, 0.75, 0, 0.25 } },
+      { key = "macros", tooltip = L["Macros"], texCoords = { 0.25, 0.5, 0, 0.25 } },
       { key = "settings", tooltip = L["Settings"], texture = "Interface\\AddOns\\"..AddonName.."\\Textures\\icons", texCoords = { 0, 0.25, 0.25, 0.5 }, iconSize = 25 },
     }
     local buttonSize = 36
