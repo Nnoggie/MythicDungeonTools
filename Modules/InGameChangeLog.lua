b23f7423f30f370c84bcd2e520ad8c6cb39b8047
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2026-05-27 17:38:11 +0200

    Release 6.1.11

--- Modules/InGameChangeLog.lua
+++ Modules/InGameChangeLog.lua
@@ -2,6 +2,15 @@ local _, MDT = ...
 
 MDT.changeLog = {
   {
+    tag = "6.1.11",
+    date = "2026-05-27",
+    notes = {
+      "Added a Combat Logging section that can automatically toggle combat logging for selected raid and dungeon difficulties.",
+      "Added an Enemy forces in tooltips setting with multiple count and percentage display formats.",
+      "Focus Marker Assignments now only shows raid subgroup 1 while in a raid group.",
+    },
+  },
+  {
     tag = "6.1.10",
     date = "2026-05-27",
     notes = {
@@ -15,13 +24,6 @@ MDT.changeLog = {
     date = "2026-05-26",
     notes = {
       "Renamed Void Infuser to Void Terror.",
-    },
-  },
-  {
-    tag = "6.1.8",
-    date = "2026-05-25",
-    notes = {
-      "Fixed the Settings section so General and Colors display side by side",
     },
   },
 }
