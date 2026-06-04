fcb46ad04051a24247a2a9d8017c7f31edbb0c3a
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2026-06-04 20:23:22 +0200

    Release 6.1.13

--- Modules/InGameChangeLog.lua
+++ Modules/InGameChangeLog.lua
@@ -2,6 +2,14 @@ local _, MDT = ...
 
 MDT.changeLog = {
   {
+    tag = "6.1.13",
+    date = "2026-06-04",
+    notes = {
+      "Fixed confirmation dialogs showing an empty checkbox when the action does not use one",
+      "Moved two bats in Maisara Caverns to more accurate positions",
+    },
+  },
+  {
     tag = "6.1.12",
     date = "2026-05-31",
     notes = {
@@ -13,15 +21,6 @@ MDT.changeLog = {
   },
   {
     tag = "6.1.11",
-    date = "2026-05-27",
-    notes = {
-      "Added a Combat Logging section that can automatically toggle combat logging for selected raid and dungeon difficulties.",
-      "Added an Enemy forces in tooltips setting with multiple count and percentage display formats.",
-      "Focus Marker Assignments now only shows raid subgroup 1 while in a raid group.",
-    },
-  },
-  {
-    tag = "6.1.10",
     date = "2026-05-27",
     notes = {
       "Added a Combat Logging section that can automatically toggle combat logging for selected raid and dungeon difficulties.",
