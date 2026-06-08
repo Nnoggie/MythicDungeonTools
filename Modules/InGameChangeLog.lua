c593996607dcc4897e62eda2b8205f416b3e15a5
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2026-06-08 21:20:15 +0200

    Release 6.1.15

--- Modules/InGameChangeLog.lua
+++ Modules/InGameChangeLog.lua
@@ -2,6 +2,13 @@ local _, MDT = ...
 
 MDT.changeLog = {
   {
+    tag = "6.1.15",
+    date = "2026-06-08",
+    notes = {
+      "Added two missing Pesty Lashlings to Windrunner Spire",
+    },
+  },
+  {
     tag = "6.1.14",
     date = "2026-06-05",
     notes = {
@@ -14,16 +21,6 @@ MDT.changeLog = {
     notes = {
       "Fixed confirmation dialogs showing an empty checkbox when the action does not use one",
       "Moved two bats in Maisara Caverns to more accurate positions",
-    },
-  },
-  {
-    tag = "6.1.12",
-    date = "2026-05-31",
-    notes = {
-      "Added a General setting to mute Xal'atath voice lines",
-      "Updated short names for Windrunner Spire, Magister's Terrace, and Nexus Point Xenas.",
-      "Adjusted Magister's Terrace G25 positions and unlinked Spellwoven Familiars",
-      "Fixed enemy count labels hiding when mouseovering enemies",
     },
   },
 }
