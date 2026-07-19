2f7a0bab018baa4b9ca94a95a2890f914720cfe0
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2026-07-19 04:40:24 +0200

    refactor: extract saved variables

---
+++ Core/SavedVariables.lua
@@ -0,0 +1,115 @@
+local AddonName, MDT = ...
+local L = MDT.L
+
+local defaultSavedVars = {
+  global = {
+    toolbarExpanded = true,
+    currentSeason = 11, -- not really used for anything anymore
+    scale = 1,
+    nonFullscreenScale = 1.4,
+    enemyForcesFormat = 2,
+    useForcesCount = false, -- replaces percent in pull buttons with count
+    showPullButtonHealth = false,
+    autoPanToPull = true,
+    enemyForcesTooltip = 1,
+    muteXalatathVoiceLines = false,
+    announceDungeonReset = false,
+    enemyStyle = 1,
+    currentDifficulty = 10,
+    xoffset = -80,
+    yoffset = -100,
+    defaultColor = "228b22",
+    anchorFrom = "TOP",
+    anchorTo = "TOP",
+    tooltipInCorner = false,
+    minimap = {
+      hide = false,
+      compartmentHide = false,
+    },
+    toolbar = {
+      color = { r = 1, g = 1, b = 1, a = 1 },
+      brushSize = 3,
+    },
+    presets = {},
+    currentPreset = {},
+    alwaysOverwriteRoutesByUID = false,
+    fadeOutDuringCombat = false,
+    fadeOutAlpha = 0.5,
+    focusMarker = {
+      announceReadyCheck = false,
+      useMacro = false,
+      preserveExistingTargetMarkers = true,
+      suppressNotifications = false,
+      assignments = {},
+    },
+    combatLogging = {
+      enabled = false,
+      content = {
+        lfr = false,
+        normal = false,
+        heroic = false,
+        mythic = false,
+        mythic_dungeon = false,
+        mythic_plus = false,
+      },
+    },
+    colorPaletteInfo = {
+      autoColoring = true,
+      forceColorBlindMode = false,
+      colorPaletteIdx = 4,
+      customPaletteValues = {},
+      numberCustomColors = 12,
+    },
+    currentDungeonIdx = MDT:IsMop() and 130 or 160, -- set this one every new season
+    currentSection = "maps",
+    latestDungeonSeen = 0,
+    selectedDungeonList = 1,
+    prePatchWarningSeenFor = 0,
+  },
+}
+
+for i = 1, 200 do
+  defaultSavedVars.global.presets[i] = {
+    [1] = {
+      text = L["Default"],
+      value = {},
+      objects = {},
+      colorPaletteInfo = { autoColoring = true, colorPaletteIdx = 4 }
+    },
+    [2] = { text = L["<New Preset>"], value = 0 },
+  }
+  defaultSavedVars.global.currentPreset[i] = 1
+end
+
+local db
+
+function MDT:GetDefaultSavedVariables()
+  return defaultSavedVars
+end
+
+function MDT:InitializeSavedVariables()
+  db = LibStub("AceDB-3.0"):New("MythicDungeonToolsDB", defaultSavedVars).global
+  if not db then return end
+
+  for dungeonIdx, presetIdx in pairs(db.currentPreset) do
+    if presetIdx <= 0 then db.currentPreset[dungeonIdx] = 1 end
+  end
+
+  return db
+end
+
+function MDT:GetDB()
+  return db
+end
+
+--/run MDT:ResetDataCache();
+function MDT:ResetDataCache()
+  db.dungeonEnemies = nil
+  db.mapPOIs = nil
+  ReloadUI()
+end
+
+function MDT:HardReset()
+  MythicDungeonToolsDB = nil
+  ReloadUI()
+end
--- MythicDungeonTools.lua
+++ MythicDungeonTools.lua
@@ -10,7 +10,8 @@ local tinsert, tremove, CreateFrame, tonumber, max, min, abs, pairs, ipairs, Get
 
 local sizex = 840
 local sizey = 555
-local defaultNonFullscreenScale = 1.4
+local defaultSavedVars = MDT:GetDefaultSavedVariables()
+local defaultNonFullscreenScale = defaultSavedVars.global.nonFullscreenScale
 local minNonFullscreenScale = 0.9
 local sidePanelWidth = 251
 local panelHeight = 30
@@ -148,94 +149,6 @@ function MDT:GetLocaleIndex()
   return localeToIndex[GetLocale()] or 1
 end
 
--------------------------
---- Saved Variables  ----
--------------------------
-local defaultSavedVars = {
-  global = {
-    toolbarExpanded = true,
-    currentSeason = 11, -- not really used for anything anymore
-    scale = 1,
-    nonFullscreenScale = defaultNonFullscreenScale,
-    enemyForcesFormat = 2,
-    useForcesCount = false, -- replaces percent in pull buttons with count
-    showPullButtonHealth = false,
-    autoPanToPull = true,
-    enemyForcesTooltip = 1,
-    muteXalatathVoiceLines = false,
-    announceDungeonReset = false,
-    enemyStyle = 1,
-    currentDifficulty = 10,
-    xoffset = -80,
-    yoffset = -100,
-    defaultColor = "228b22",
-    anchorFrom = "TOP",
-    anchorTo = "TOP",
-    tooltipInCorner = false,
-    minimap = {
-      hide = false,
-      compartmentHide = false,
-    },
-    toolbar = {
-      color = { r = 1, g = 1, b = 1, a = 1 },
-      brushSize = 3,
-    },
-    presets = {},
-    currentPreset = {},
-    alwaysOverwriteRoutesByUID = false,
-    fadeOutDuringCombat = false,
-    fadeOutAlpha = 0.5,
-    focusMarker = {
-      announceReadyCheck = false,
-      useMacro = false,
-      preserveExistingTargetMarkers = true,
-      suppressNotifications = false,
-      assignments = {},
-    },
-    combatLogging = {
-      enabled = false,
-      content = {
-        lfr = false,
-        normal = false,
-        heroic = false,
-        mythic = false,
-        mythic_dungeon = false,
-        mythic_plus = false,
-      },
-    },
-    colorPaletteInfo = {
-      autoColoring = true,
-      forceColorBlindMode = false,
-      colorPaletteIdx = 4,
-      customPaletteValues = {},
-      numberCustomColors = 12,
-    },
-    currentDungeonIdx = MDT:IsMop() and 130 or 160, -- set this one every new season
-    currentSection = "maps",
-    latestDungeonSeen = 0,
-    selectedDungeonList = 1,
-    prePatchWarningSeenFor = 0,
-  },
-}
-do
-  for i = 1, 200 do
-    defaultSavedVars.global.presets[i] = {
-      [1] = {
-        text = L["Default"],
-        value = {},
-        objects = {},
-        colorPaletteInfo = { autoColoring = true, colorPaletteIdx = 4 }
-      },
-      [2] = { text = L["<New Preset>"], value = 0 },
-    }
-    defaultSavedVars.global.currentPreset[i] = 1
-  end
-end
-
-function MDT:GetDefaultSavedVariables()
-  return defaultSavedVars
-end
-
 -- Init db
 local eventFrame
 do
@@ -249,7 +162,7 @@ do
 
   function MDT.ADDON_LOADED(self, addon)
     if addon == "MythicDungeonTools" then
-      db = LibStub("AceDB-3.0"):New("MythicDungeonToolsDB", defaultSavedVars).global
+      db = MDT:InitializeSavedVariables()
       if not db then return end
       ---@diagnostic disable-next-line: param-type-mismatch
       minimapIcon:Register("MythicDungeonTools", LDB, db.minimap)
@@ -257,12 +170,6 @@ do
       --compartment
       if not db.minimap.compartmentHide then
         minimapIcon:AddButtonToCompartment("MythicDungeonTools")
-      end
-      --fix db corruption
-      do
-        for k, v in pairs(db.currentPreset) do
-          if v <= 0 then db.currentPreset[k] = 1 end
-        end
       end
       -- Initialize fade frame for combat transparency
       MDT:InitializeFadeFrame()
@@ -334,10 +241,6 @@ end
 
 function MDT:GetDungeonSublevels()
   return MDT.dungeonSubLevels
-end
-
-function MDT:GetDB()
-  return db
 end
 
 function MDT:RegisterMainFrameDragHandle(dragHandle, frame)
@@ -4077,18 +3980,6 @@ function MDT:UpdatePullButtonColor(pullIdx, r, g, b)
   if not button then return end
   button.color.r, button.color.g, button.color.b = r, g, b
   button:UpdateColor()
-end
-
---/run MDT:ResetDataCache();
-function MDT:ResetDataCache()
-  db.dungeonEnemies = nil
-  db.mapPOIs = nil
-  ReloadUI()
-end
-
-function MDT:HardReset()
-  MythicDungeonToolsDB = nil
-  ReloadUI()
 end
 
 MDT.modules = {}
