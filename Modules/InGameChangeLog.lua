9cf9263e757faab9a56fbf269da1f9f884a4407c
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2026-08-18 19:08:10 +0200

    Release 6.2.4

--- Modules/InGameChangeLog.lua
+++ Modules/InGameChangeLog.lua
@@ -2,6 +2,13 @@ local _, MDT = ...
 
 MDT.changeLog = {
   {
+    tag = "6.2.4",
+    date = "2026-08-18",
+    notes = {
+      "Corrected Murder Row's total enemy forces from 690 to 655.",
+    },
+  },
+  {
     tag = "6.2.3",
     date = "2026-08-17",
     notes = {
@@ -15,18 +22,6 @@ MDT.changeLog = {
     date = "2026-08-14",
     notes = {
       "Corrected enemy data and map positions for Altar of Fangs, Den of Nalorakk, Murder Row, and The Blinding Vale.",
-    },
-  },
-  {
-    tag = "6.2.1",
-    date = "2026-08-11",
-    notes = {
-      "Add WoW 12.1 support for Midnight Season 2",
-      "Added an option to show each pull's total health in the route sidebar. (off by default)",
-      "Added an option to automatically pan the map to the selected pull. (on by default)",
-      "Enemies can now be dragged on the map to move them into pulls, with a preview showing which enemies will move.",
-      "New route exports and group sharing now use Blizzard's route encoding.",
-      "MDT's interface now loads only when opened, reducing memory usage while MDT is not in use and reducing loading screen impact.",
     },
   },
 }
