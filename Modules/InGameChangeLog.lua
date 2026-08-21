68e9378b7e894ebdf990f8759657f70c611742fa
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2026-08-21 16:42:37 +0200

    Release 6.2.5

--- Modules/InGameChangeLog.lua
+++ Modules/InGameChangeLog.lua
@@ -2,6 +2,19 @@ local _, MDT = ...
 
 MDT.changeLog = {
   {
+    tag = "6.2.5",
+    date = "2026-08-21",
+    notes = {
+      "Corrected enemy groupings, names, boss data, and map positions multiple dungeons",
+      "Corrected Temple of Sethraliss's total enemy forces from 649 to 689.",
+      "Corrected enemy health values",
+      "Added / corrected dungeon entrance markers",
+      "Corrected Spanish dungeon short names",
+      "Automatic combat logging no longer posts start and stop messages in chat.",
+      "Selecting no Focus Marker no longer clears an existing target marker when \"Don't overwrite existing target markers\" is enabled.",
+    },
+  },
+  {
     tag = "6.2.4",
     date = "2026-08-18",
     notes = {
@@ -15,13 +28,6 @@ MDT.changeLog = {
       "Improved map loading and responsiveness when displaying large numbers of enemies.",
       "Corrected enemy positions and icon sizes throughout The Blinding Vale.",
       "Bosses in The Blinding Vale no longer contribute enemy forces.",
-    },
-  },
-  {
-    tag = "6.2.2",
-    date = "2026-08-14",
-    notes = {
-      "Corrected enemy data and map positions for Altar of Fangs, Den of Nalorakk, Murder Row, and The Blinding Vale.",
     },
   },
 }
