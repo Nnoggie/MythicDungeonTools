947c4ea8b2105c674c32eb94255ee01c21813144
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2026-08-22 13:27:24 +0200

    Release 6.2.6

--- Modules/InGameChangeLog.lua
+++ Modules/InGameChangeLog.lua
@@ -2,6 +2,13 @@ local _, MDT = ...
 
 MDT.changeLog = {
   {
+    tag = "6.2.6",
+    date = "2026-08-22",
+    notes = {
+      "Corrected Temple of Sethraliss's total enemy forces from 689 to 649.",
+    },
+  },
+  {
     tag = "6.2.5",
     date = "2026-08-21",
     notes = {
@@ -19,15 +26,6 @@ MDT.changeLog = {
     date = "2026-08-18",
     notes = {
       "Corrected Murder Row's total enemy forces from 690 to 655.",
-    },
-  },
-  {
-    tag = "6.2.3",
-    date = "2026-08-17",
-    notes = {
-      "Improved map loading and responsiveness when displaying large numbers of enemies.",
-      "Corrected enemy positions and icon sizes throughout The Blinding Vale.",
-      "Bosses in The Blinding Vale no longer contribute enemy forces.",
     },
   },
 }
