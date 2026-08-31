e214023441c425a6be2ecea33a5ceb0fc3b87d19
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2026-08-31 15:52:25 +0200

    Release 6.2.10

--- Modules/InGameChangeLog.lua
+++ Modules/InGameChangeLog.lua
@@ -2,6 +2,19 @@ local _, MDT = ...
 
 MDT.changeLog = {
   {
+    tag = "6.2.10",
+    date = "2026-08-31",
+    notes = {
+      "Updated enemy health in S2 dungeons.",
+      "Added missing abilities for Shadow of Zul in King's Rest, Adderis and Faithless Subjugators in Temple of Sethraliss, and Thorny Saptors in The Blinding Vale.",
+      "Corrected enemy models in Den of Nalorakk, Temple of Sethraliss, and Voidscar Arena, along with the creature types of Massive Felwyrm and Brutok and Aegyra the Unyielding's level.",
+      "Removed extra Earthwhisper Tenders and corrected pack groupings in Den of Nalorakk.",
+      "Removed an extra High Evolutionist after the first boss in Altar of Fangs.",
+      "Corrected Temple of Sethraliss G30's enemy forces to 12 per Imbued Stormcaller and 30 for its Agitated Nimbus.",
+      "Replaced one Faithless Subjugator in Temple of Sethraliss G23 with a Lightning Serpent.",
+    },
+  },
+  {
     tag = "6.2.9",
     date = "2026-08-25",
     notes = {
@@ -18,13 +31,6 @@ MDT.changeLog = {
       "Corrected pull groupings and floor assignments in The Blinding Vale, Den of Nalorakk, Murder Row, and Voidscar Arena.",
       "Added missing enemy placements in The Blinding Vale, Den of Nalorakk, and Murder Row.",
       "Corrected the models for Ula'tek's Chosen, Twinfang Harrower, and Blade of the Altar in Altar of Fangs.",
-    },
-  },
-  {
-    tag = "6.2.7",
-    date = "2026-08-22",
-    notes = {
-      "Corrected Temple of Sethraliss's total enemy forces from 649 to 689.",
     },
   },
 }
