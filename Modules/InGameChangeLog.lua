c0bb2948ce016f5a2b996402cb7144bcaa4493fd
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2026-09-04 13:46:51 +0200

    Release 6.2.13

--- Modules/InGameChangeLog.lua
+++ Modules/InGameChangeLog.lua
@@ -2,6 +2,13 @@ local _, MDT = ...
 
 MDT.changeLog = {
   {
+    tag = "6.2.13",
+    date = "2026-09-04",
+    notes = {
+      "Corrected pull groupings for Lightfeather Petalwings and a Sporeblight Belcher, and removed an extra Radiant Spellsower in The Blinding Vale.",
+    },
+  },
+  {
     tag = "6.2.12",
     date = "2026-09-03",
     notes = {
@@ -14,19 +21,6 @@ MDT.changeLog = {
     notes = {
       "Corrected enemy placements and pull groupings in King's Rest, Ruby Life Pools, Temple of Sethraliss, and The Blinding Vale.",
       "Restored developer-mode macros for adding enemies and patrol waypoints.",
-    },
-  },
-  {
-    tag = "6.2.10",
-    date = "2026-08-31",
-    notes = {
-      "Updated enemy health in S2 dungeons.",
-      "Added missing abilities for Shadow of Zul in King's Rest, Adderis and Faithless Subjugators in Temple of Sethraliss, and Thorny Saptors in The Blinding Vale.",
-      "Corrected enemy models in Den of Nalorakk, Temple of Sethraliss, and Voidscar Arena, along with the creature types of Massive Felwyrm and Brutok and Aegyra the Unyielding's level.",
-      "Removed extra Earthwhisper Tenders and corrected pack groupings in Den of Nalorakk.",
-      "Removed an extra High Evolutionist after the first boss in Altar of Fangs.",
-      "Corrected Temple of Sethraliss G30's enemy forces to 12 per Imbued Stormcaller and 30 for its Agitated Nimbus.",
-      "Replaced one Faithless Subjugator in Temple of Sethraliss G23 with a Lightning Serpent.",
     },
   },
 }
