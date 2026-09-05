1cbb06bab1bc3dbed091d1e1a5d2157e553c10a2
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2026-09-05 19:08:31 +0200

    Release 6.2.15

--- Modules/InGameChangeLog.lua
+++ Modules/InGameChangeLog.lua
@@ -2,6 +2,13 @@ local _, MDT = ...
 
 MDT.changeLog = {
   {
+    tag = "6.2.15",
+    date = "2026-09-05",
+    notes = {
+      "Added two missing Twinfang Harrowers in Altar of Fangs.",
+    },
+  },
+  {
     tag = "6.2.14-alpha1",
     date = "2026-09-05",
     notes = {
@@ -14,13 +21,6 @@ MDT.changeLog = {
     date = "2026-09-04",
     notes = {
       "Corrected pull groupings for Lightfeather Petalwings and a Sporeblight Belcher, and removed an extra Radiant Spellsower in The Blinding Vale.",
-    },
-  },
-  {
-    tag = "6.2.12",
-    date = "2026-09-03",
-    notes = {
-      "Fixed an error when creating the MDT Focus Marker macro.",
     },
   },
 }
