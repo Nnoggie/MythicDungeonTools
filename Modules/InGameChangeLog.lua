952d152a8694bbde8421dc14995522f853515886
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2026-09-08 21:21:39 +0200

    Release 6.2.16

--- Modules/InGameChangeLog.lua
+++ Modules/InGameChangeLog.lua
@@ -2,6 +2,14 @@ local _, MDT = ...
 
 MDT.changeLog = {
   {
+    tag = "6.2.16",
+    date = "2026-09-08",
+    notes = {
+      "Added a missing Lightgorged Lasher and corrected a Lasher's position in The Blinding Vale.",
+      "Added missing translations for The Blinding Vale enemies and Thundering Totem in King's Rest.",
+    },
+  },
+  {
     tag = "6.2.15",
     date = "2026-09-05",
     notes = {
@@ -14,13 +22,6 @@ MDT.changeLog = {
     notes = {
       "Experimental alpha version intended to help debug route sharing issues.",
       "Added detailed error reports for route sharing problems. If an MDT error window appears while sharing a route, please report it using the links in the window.",
-    },
-  },
-  {
-    tag = "6.2.13",
-    date = "2026-09-04",
-    notes = {
-      "Corrected pull groupings for Lightfeather Petalwings and a Sporeblight Belcher, and removed an extra Radiant Spellsower in The Blinding Vale.",
     },
   },
 }
