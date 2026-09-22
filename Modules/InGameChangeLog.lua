20f3b4d4b9806b9699a911595ee01ee02e2c5136
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2026-09-22 02:55:53 +0200

    Release 6.2.19

--- Modules/InGameChangeLog.lua
+++ Modules/InGameChangeLog.lua
@@ -2,6 +2,14 @@ local _, MDT = ...
 
 MDT.changeLog = {
   {
+    tag = "6.2.19",
+    date = "2026-09-22",
+    notes = {
+      "Fixed overlapping enemy blips visuals",
+      "Corrected G34 Living Venom positions and added two missing Living Venoms in Altar of Fangs.",
+    },
+  },
+  {
     tag = "6.2.18",
     date = "2026-09-21",
     notes = {
@@ -14,14 +22,6 @@ MDT.changeLog = {
     notes = {
       "Fix automatic dungeon selection in shared zones.",
       "Improve Korean translations throughout MDT.",
-    },
-  },
-  {
-    tag = "6.2.16",
-    date = "2026-09-08",
-    notes = {
-      "Added a missing Lightgorged Lasher and corrected a Lasher's position in The Blinding Vale.",
-      "Added missing translations for The Blinding Vale enemies and Thundering Totem in King's Rest.",
     },
   },
 }
