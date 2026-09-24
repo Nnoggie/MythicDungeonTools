7fd7672d41b3aff933cbfd5b0f508fc4f62e33ae
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2026-09-25 01:28:50 +0200

    Release 6.2.20

--- Modules/InGameChangeLog.lua
+++ Modules/InGameChangeLog.lua
@@ -2,6 +2,16 @@ local _, MDT = ...
 
 MDT.changeLog = {
   {
+    tag = "6.2.20",
+    date = "2026-09-25",
+    notes = {
+      "Backend changes",
+      "Added a Mutating Elixir map icon in Altar of Fangs.",
+      "Added Warding Incense and Rune of Anchoring map icons in Den of Nalorakk.",
+      "Added Proof of Endurance and Proof of Mastery map icons above the enemies that grant them in Voidscar Arena.",
+    },
+  },
+  {
     tag = "6.2.19",
     date = "2026-09-22",
     notes = {
@@ -14,14 +24,6 @@ MDT.changeLog = {
     date = "2026-09-21",
     notes = {
       "Corrected positions of G62 and a Devouring Brutalizer in Voidscar Arena.",
-    },
-  },
-  {
-    tag = "6.2.17",
-    date = "2026-09-21",
-    notes = {
-      "Fix automatic dungeon selection in shared zones.",
-      "Improve Korean translations throughout MDT.",
     },
   },
 }
